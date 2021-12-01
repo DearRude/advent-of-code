13c85181f100d4dd55563e459440ba3a8260cd55
Ebrahim <dearrude@tfwno.gf>
2021-12-01 17:51:51 +0330

    Add 2021/day01 in Zig

---
+++ 2021/day-01/part-2.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+const input = @embedFile("input.txt");
+
+pub fn main() !void {
+    var lines = std.mem.tokenize(input, "\n");
+    var nums = std.ArrayList(u32).init(std.testing.allocator);
+    defer nums.deinit();
+    while (lines.next()) |line| {
+        try nums.append(try std.fmt.parseInt(u32, line, 10));
+    }
+    var increases: u32 = 0;
+    var i: u32 = 3;
+    while (i < nums.items.len) : (i += 1) {
+        if (nums.items[i - 3] < nums.items[i]) {
+            increases += 1;
+        }
+    }
+
+    std.debug.print("{}\n", .{increases});
+}
